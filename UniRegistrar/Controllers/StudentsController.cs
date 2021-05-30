d1d127075e99f89c12d3ba70b18b20d1c66680ab
Collin Chapman <cchap14@gmail.com>
2021-05-30 11:54:09 -0700

    add the following routes and views for Student: details, edit, create and addcourse
    Co-authored-by: Shannon Lee <shannonleehj@gmail.com>

--- UniRegistrar/Controllers/StudentsController.cs
+++ UniRegistrar/Controllers/StudentsController.cs
@@ -24,10 +24,61 @@ namespace UniRegistrar.Controllers
     }
     public ActionResult Create()
     {
-      // List<Course> courseList= _db.Courses.ToList();
-      // ViewBag.CourseList = courseList;
-      ViewBag.CourseId = new MultiSelectList(_db.Courses, "CourseId", "CourseName");
+      ViewBag.CourseId = new SelectList(_db.Courses, "CourseId", "CourseName");
       return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Student student, int CourseId)
+    {
+      _db.Students.Add(student);
+      _db.SaveChanges();
+      if (CourseId != 0)
+      {
+        _db.CourseStudent.Add(new CourseStudent() { CourseId = CourseId, StudentId = student.StudentId });
+      }
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult Details(int id)
+    {
+      var thisStudent = _db.Students
+      .Include(student => student.JoinEntities)
+      .ThenInclude(join => join.Course)
+      .FirstOrDefault(student => student.StudentId == id);
+      return View(thisStudent);
+    }
+    public ActionResult Edit(int id)
+    {
+      var thisStudent = _db.Students.FirstOrDefault(student => student.StudentId == id);
+      ViewBag.CourseId = new SelectList(_db.Courses, "CourseId", "CourseName");
+      return View(thisStudent);
+    }
+    [HttpPost]
+    public ActionResult Edit(Student student)
+    {
+      _db.Entry(student).State = EntityState.Modified;
+      _db.SaveChanges();
+      return RedirectToAction("Details", new {id = student.StudentId});
+    }
+
+    public ActionResult AddCourse(int id)
+    {
+      var thisStudent = _db.Students.FirstOrDefault(student => student.StudentId == id);
+      ViewBag.CourseId = new SelectList(_db.Courses, "CourseId", "CourseName");
+      return View(thisStudent);
+    }
+
+    [HttpPost]
+    public ActionResult AddCourse(Student student, int CourseId)
+    {
+      if (CourseId != 0)
+      {
+        _db.CourseStudent.Add(new CourseStudent() { CourseId = CourseId, StudentId = student.StudentId });
+      }
+      _db.SaveChanges();
+      return RedirectToAction("Details", new {id = student.StudentId});
     }
   }
 }
