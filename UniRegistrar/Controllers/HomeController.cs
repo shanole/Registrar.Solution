32adf7c479ffff1de29493d9e479ac041c31ba7a
Collin Chapman <cchap14@gmail.com>
2021-06-02 19:33:24 -0700

    add functionality to list all instances of student and courses table to the splash page
    Co-authored-by: Shannon Lee <shannonleehj@gmail.com>
    Co-Authored-by: Salim Mayan <mailsalim@gmail.com>

--- UniRegistrar/Controllers/HomeController.cs
+++ UniRegistrar/Controllers/HomeController.cs
@@ -1,12 +1,29 @@
 using Microsoft.AspNetCore.Mvc;
+using UniRegistrar.Models;
+using System.Collections.Generic;
+using System.Linq;
+
 
 namespace UniRegistrar.Controllers
 {
   public class HomeController : Controller
   {
+    private readonly UniRegistrarContext _db;
+
+    public HomeController(UniRegistrarContext db)
+    {
+      _db = db;
+    }
+
     [HttpGet("/")]
     public ActionResult Index()
     {
+      // IEnumerable<Student> sortedStudents = _db.Students.OrderBy(student => student.StudentName);
+      // ViewBag.StudentObj = sortedStudents.ToList();
+      ViewBag.StudentObj = new List<Student>(_db.Students.OrderBy(student => student.StudentName));
+
+      IEnumerable<Course> sortedCourses = _db.Courses.OrderBy(course => course.CourseName);
+      ViewBag.CourseObj = sortedCourses.ToList();
       return View();
     }
   }
